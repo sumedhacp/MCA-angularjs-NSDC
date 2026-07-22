91501a396c6d853d9a194be57027aaba11b4bb4c
sumedhacp <your-sumedhacp-email@example.com>
2026-07-22 11:39:14 +0530

    fileter and ng-switch coded

--- js/app.js
+++ js/app.js
@@ -2,22 +2,44 @@ var app = angular.module('campus360', []);
 
 app.controller("dashboardcontroller", function ($scope) {
 
-    $scope.collegename = "FISAT";
+    $scope.collegename = "FISaT";
     $scope.totalstudents = 1200;
     $scope.totalfaculty = 85;
 
     $scope.students = [
+        "Sumedha",
+        "Smruthi",
+        "Hiba",
+        "Nazeeya"
+    ];
 
-        "sumedha", "smruthi", "hiba", "nazeeya"
+    $scope.showstudents = false;
+    $scope.showDashboard = true;
+
+    $scope.fees = 25600;
+    $scope.today = new Date();
+
+    $scope.departments=[
+
+        "CSE",
+        "MCA",
+        "MBA",
+        "BBA",
+        "EEE",
+        "IMCA"
 
     ];
+
+$scope.selectedDepartment = "MCA";
 
     $scope.addstudent = function () {
 
         $scope.totalstudents++;
-
     };
 
-    console.log("dashboard controller is loaded");
-});
+    $scope.addfaculty = function () {
+        $scope.totalfaculty++;
+    };
 
+    console.log("Dashboard controller is loaded");
+});
